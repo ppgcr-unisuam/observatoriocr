49da8d6ae31267a2168b6ddb9418d223347eee96
Arthur Ferreira <arthur_sf@icloud.com>
2022-01-19 17:08:40 -0300

    Commit.

--- Scripts/read_sucupira.R
+++ Scripts/read_sucupira.R
@@ -1,10 +1,18 @@
-sucupira.raw <- c()
+# load list of Sucupira files to read
 files.to.read <-
-  list.files(file.path(getwd(), "Sucupira"),
-             pattern = "xlsx",
-             full.names = TRUE,
-             recursive = TRUE)
+  list.files(
+    file.path(getwd(), "Sucupira"),
+    pattern = "xlsx",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
+# initialize vector and list
+sucupira.raw <- c()
+sucupira.list <- list()
+
 for (i in 1:length(files.to.read)) {
   sucupira.raw <-
     rbind(sucupira.raw, read_excel(files.to.read[i], sheet = sheet))
-  }
+  sucupira.list[[i]] <- read_excel(files.to.read[i], sheet = sheet)
+}
--- docs/Scripts/docs_to_wordcloud.R
+++ docs/Scripts/docs_to_wordcloud.R
@@ -1,3 +1,6 @@
+# for reproducibility
+set.seed(0)
+
 # create a corpus
 docs <- Corpus(VectorSource(data.to.cloud))
 
@@ -7,7 +10,8 @@ docs <- docs %>%
   tm_map(removePunctuation) %>%
   tm_map(stripWhitespace)
 docs <- tm_map(docs, content_transformer(tolower))
-docs <- tm_map(docs, removeWords, stopwords("portuguese"))
+tryCatch(docs <- tm_map(docs, removeWords, stopwords("portuguese")))
+tryCatch(docs <- tm_map(docs, removeWords, stopwords("english")))
 
 # create a document-term matrix
 dtm <- TermDocumentMatrix(docs)
@@ -15,15 +19,33 @@ matrix <- as.matrix(dtm)
 words <- sort(rowSums(matrix), decreasing = TRUE)
 df <- data.frame(word = names(words), freq = words)
 
-# generate word cloud
-par(bg = "#2C3E50")
-set.seed(0) # for reproducibility
-wordcloud(
-  words = df$word,
-  freq = df$freq,
-  min.freq = 1,
-  max.words = 200,
-  random.order = FALSE,
-  rot.per = 0.35,
-  colors = brewer.pal(8, "Set3")
+# set minimum word frequency
+df <- df[df$freq >= 1, ]
+
+# set plot area
+par(
+  oma = c(0, 0, 0, 0),
+  mar = c(0, 0, 0, 0),
+  bg = "#2C3E50"
 )
+# generate word cloud
+wordcloud <- wordcloud2(
+  data = df,
+  size = 0.5,
+#  color = rev(viridis(length(unique(df$freq)))),
+  color = colorRampPalette(brewer.pal(11, "Blues"))(diff(range(df$freq))),
+  backgroundColor = "#2C3E50",
+  shuffle = FALSE,
+  rotateRatio = 0,
+  ellipticity = 0.5
+)
+
+# save it in html
+saveWidget(wordcloud, file.path(dir.path, "tmp.html"), selfcontained = F)
+
+# and in png
+webshot(file.path(dir.path, "tmp.html"), file.path(dir.path, paste0(sheet, ".png")), delay = 5, vwidth = 600, vheight = 600)
+
+# delete the tmp folder and file
+unlink(file.path(dir.path, "tmp_files"), recursive = TRUE)
+unlink(file.path(dir.path, "tmp.html"))

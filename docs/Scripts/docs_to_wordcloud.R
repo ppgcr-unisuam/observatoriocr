02c9914c09590d9787c3051a5d0e9c5a50b3dded
Arthur Ferreira <arthur_sf@icloud.com>
2022-01-19 21:02:56 -0300

    Commit.

--- docs/Scripts/docs_to_wordcloud.R
+++ docs/Scripts/docs_to_wordcloud.R
@@ -10,8 +10,12 @@ docs <- docs %>%
   tm_map(removePunctuation) %>%
   tm_map(stripWhitespace)
 docs <- tm_map(docs, content_transformer(tolower))
-tryCatch(docs <- tm_map(docs, removeWords, stopwords("portuguese")))
-tryCatch(docs <- tm_map(docs, removeWords, stopwords("english")))
+tryCatch(
+  docs <- tm_map(docs, removeWords, stopwords("portuguese"))
+)
+tryCatch(
+  docs <- tm_map(docs, removeWords, stopwords("english"))
+)
 
 # create a document-term matrix
 dtm <- TermDocumentMatrix(docs)
@@ -20,19 +24,17 @@ words <- sort(rowSums(matrix), decreasing = TRUE)
 df <- data.frame(word = names(words), freq = words)
 
 # set minimum word frequency
-df <- df[df$freq >= 1, ]
+df <- df[df$freq >= 1,]
 
 # set plot area
-par(
-  oma = c(0, 0, 0, 0),
-  mar = c(0, 0, 0, 0),
-  bg = "#2C3E50"
-)
+par(oma = c(0, 0, 0, 0),
+    mar = c(0, 0, 0, 0),
+    bg = "#2C3E50")
 # generate word cloud
 wordcloud <- wordcloud2(
   data = df,
   size = 0.5,
-#  color = rev(viridis(length(unique(df$freq)))),
+  #  color = rev(viridis(length(unique(df$freq)))),
   color = colorRampPalette(brewer.pal(11, "Blues"))(diff(range(df$freq))),
   backgroundColor = "#2C3E50",
   shuffle = FALSE,
@@ -44,7 +46,13 @@ wordcloud <- wordcloud2(
 saveWidget(wordcloud, file.path(dir.path, "tmp.html"), selfcontained = F)
 
 # and in png
-webshot(file.path(dir.path, "tmp.html"), file.path(dir.path, paste0(sheet, ".png")), delay = 5, vwidth = 600, vheight = 600)
+webshot(
+  file.path(dir.path, "tmp.html"),
+  file.path(dir.path, paste0(sheet, ".png")),
+  delay = 5,
+  vwidth = 600,
+  vheight = 600
+)
 
 # delete the tmp folder and file
 unlink(file.path(dir.path, "tmp_files"), recursive = TRUE)

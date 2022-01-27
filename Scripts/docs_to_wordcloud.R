44bccc4728ebbbf5e544148146cfa547dadfad3a
Arthur Ferreira <arthur_sf@icloud.com>
2022-01-27 14:30:45 -0300

    Commit.

--- Scripts/docs_to_wordcloud.R
+++ Scripts/docs_to_wordcloud.R
@@ -40,7 +40,7 @@ par(
 wordcloud <- wordcloud2(
   data = df,
   size = 0.5,
-  color = colorRampPalette(brewer.pal(9, "Blues"))(diff(range(df$freq))),
+  color = colorRampPalette(brewer.pal(9, "Blues"))(length(df$freq)),
   backgroundColor = "#2C3E50",
   shuffle = FALSE,
   rotateRatio = 0,

a754e3da187661d6ea86765a0b78c61654313e13
Arthur Ferreira <arthur_sf@icloud.com>
2022-02-14 11:33:26 -0300

    Commit.

--- docs/Scripts/docs_to_wordcloud.R
+++ docs/Scripts/docs_to_wordcloud.R
@@ -40,7 +40,7 @@ par(
 wordcloud <- wordcloud2(
   data = df,
   size = 0.5,
-  color = rep(brewer.pal(n = 9, name = "Set3"), length.out = df$freq),
+  color = rep(brewer.pal(n = 9, name = "Set3"), length.out = length(df$freq)),
   backgroundColor = "#2C3E50",
   shuffle = FALSE,
   rotateRatio = 0,

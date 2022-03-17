46ce86b12ffe6c3fb112d0878743891a7897aa88
Arthur Ferreira <arthur_sf@icloud.com>
2022-03-17 17:38:46 -0300

    Commit.

---
+++ Scripts/line_break.R
@@ -0,0 +1,15 @@
+broken_text <-text.2.break
+for(j in 1:length(text.2.break)){
+  k <- c()
+  for(i in 1:length(strsplit(text.2.break[j], split = " ")[[1]])){
+    x <- nchar(paste0(strsplit(text.2.break[j], split = " ")[[1]][1:i], collapse = ""))
+    k <- c(k, x)
+  }
+  index <- sum(k <= ceiling(nchar(text.2.break[j])/2))
+  broken_text[j] <-
+    paste0(
+      paste(strsplit(text.2.break[j], split = " ")[[1]][1:index], collapse = " "),
+      "\n",
+      paste(strsplit(text.2.break[j], split = " ")[[1]][(index+1):length(strsplit(text.2.break[j], split = " ")[[1]])], collapse = " ")
+  )
+}

9910fa125c1caf280b749bfa2b68db86d1c362fa
Arthur Ferreira <arthur_sf@icloud.com>
2022-04-24 12:18:34 -0300

    Commit.

--- Scripts/all-required-packages.R
+++ Scripts/all-required-packages.R
@@ -1,112 +1,54 @@
 # all packages required for this R project
 # Create using PACKUP add-in to generate this code with all the required libraries for this Rmd
-if (!require(alluvial))
-  install.packages(alluvial) # for alluvial plots
 
-if (!require(anytime))
-  install.packages(anytime) #
+packs <-
+  c(
+    "alluvial",
+    "anytime",
+    "bsplus",
+    "cowplot",
+    "dplyr",
+    "DT",
+    "fontawesome",
+    "geobr",
+    "ggplot2",
+    "ggpubr",
+    "grid",
+    "gridExtra",
+    "gtsummary",
+    "hrbrthemes",
+    "htmlwidgets",
+    "httpuv",
+    "igraph",
+    "janitor",
+    "kableExtra",
+    "knitr",
+    "lubridate",
+    "magrittr",
+    "plyr",
+    "rAltmetric",
+    "RColorBrewer",
+    "rcrossref",
+    "readtext",
+    "readxl",
+    "Rmisc",
+    "rorcid",
+    "sf",
+    "stringr",
+    "tidyverse",
+    "tm",
+    "tools",
+    "usethis",
+    "vioplot",
+    "webshot2",
+    "wordcloud2"
+  )
 
-if (!require(bsplus))
-  install.packages(bsplus) # for carousel outputs
+for (i in 1:length(packs)) {
+  if (!require(packs[i], character.only = TRUE, quietly = TRUE))
+    install.packages(packs[i], character.only = TRUE)
+}
 
-if (!require(cowplot))
-  install.packages(cowplot) # for mutipannel plots
-
-if (!require(dplyr))
-  install.packages(dplyr) # for string manipulation
-
-if (!require(fontawesome))
-  install.packages(fontawesome) # for Font Awesome icons
-
-if (!require(geobr))
-  install.packages(geobr) # for BR geospacial data
-
-if (!require(ggplot2))
-  install.packages(ggplot2) # for gg_style plotting
-
-if (!require(ggpubr))
-  install.packages(ggpubr) # for
-
-if (!require(grid))
-  install.packages(grid) # for mutipannel plots
-
-if (!require(gridExtra))
-  install.packages(gridExtra) # for mutipannel plots
-
-if (!require(gtsummary))
-  install.packages(gtsummary) # for summary tables
-
-if (!require(hrbrthemes))
-  install.packages(hrbrthemes) # for saving HTML as images
-
-if (!require(htmlwidgets))
-  install.packages(htmlwidgets) # for saving HTML as images
-
-if (!require(httpuv))
-  install.packages(httpuv) #
-
-if (!require(igraph))
-  install.packages(igraph) # for network analysis
-
-if (!require(janitor))
-  install.packages(janitor) #
-
-if (!require(kableExtra))
-  install.packages(kableExtra) # for printing tables
-
-if (!require(knitr))
-  install.packages(knitr) # for
-
-if (!require(lubridate))
-  install.packages(lubridate) #
-
-if (!require(magrittr))
-  install.packages(magrittr) # for
-
-if (!require(plyr))
-  install.packages(plyr) # for getting Altmetric API
-
-if (!require(rAltmetric))
-  install.packages(rAltmetric) # for getting Altmetric API
-
-if (!require(RColorBrewer))
-  install.packages(RColorBrewer) # for custom color palettes
-
-if (!require(rcrossref))
-  install.packages(rcrossref) #
-
-if (!require(readtext))
-  install.packages(readtext) # for reading TXT files
-
-if (!require(readxl))
-  install.packages(readxl) # for reading Excel files
-
-if (!require(Rmisc))
-  install.packages(Rmisc) # for
-
-if (!require(rorcid))
-  install.packages(rorcid) # for reading ORCID data
-
-if (!require(sf))
-  install.packages(sf) # for plotting geospacial data
-
-if (!require(tidyverse))
-  install.packages(tidyverse) # for getting Altmetric API
-
-if (!require(tm))
-  install.packages(tm) # for text manipulation
-
-if (!require(tools))
-  install.packages(tools) #
-
-if (!require(usethis))
-  install.packages(usethis) #
-
-if (!require(vioplot))
-  install.packages(vioplot) # for violin plots
-
-if (!require(webshot2))
-  install.packages(webshot2) # for saving HTML as images
-
-if (!require(wordcloud2))
-  install.packages(wordcloud2) # for generating wordclouds
+for (i in 1:length(packs)) {
+  library(packs[i], character.only = TRUE)
+}
--- Scripts/read-xlsx-sucupira.R
+++ Scripts/read-xlsx-sucupira.R
@@ -1,11 +1,22 @@
+# list all directories to extract Sucupira (yearly) data
+dirs <-
+  list.dirs(file.path(getwd(), "Sucupira"),
+            full.names = FALSE,
+            recursive = FALSE)
+
 # load list of Sucupira files to read
-files.to.read <-
-  list.files(
-    file.path(getwd(), "Sucupira"),
-    pattern = "xlsx",
-    full.names = TRUE,
-    recursive = TRUE
+files.to.read <- c()
+for (i in 1:length(dirs)) {
+  files.to.read <- c(
+    files.to.read,
+    list.files(
+      file.path(getwd(), "Sucupira", dirs[i]),
+      pattern = "xlsx",
+      full.names = TRUE,
+      recursive = FALSE
+    )
   )
+}
 
 # initialize vectors and lists
 sucupira <- c()

5e7985cb8ef28dee4636693cf17f22060841f38b
Arthur Ferreira <arthur_sf@icloud.com>
2022-05-20 14:38:40 -0300

    Commit.

--- docs/Scripts/all-required-packages.R
+++ docs/Scripts/all-required-packages.R
@@ -4,6 +4,8 @@
 # 2. library(packup)
 # 3. packup()
 
+# most packages work fine if installed from CRAN
+
 packs <-
   c(
     "alluvial",
@@ -11,10 +13,10 @@ packs <-
     "bsplus",
     "cowplot",
     "details",
+    "devtools",
     "dplyr",
     "DT",
     "fontawesome",
-    "geobr",
     "ggplot2",
     "ggpubr",
     "grid",
@@ -30,13 +32,12 @@ packs <-
     "lemon",
     "lubridate",
     "magrittr",
-    "packup",
     "plyr",
     "rAltmetric",
     "RColorBrewer",
-    "rcrossref",
     "readtext",
     "readxl",
+    "rmarkdown",
     "Rmisc",
     "rorcid",
     "sf",
@@ -54,6 +55,19 @@ for (i in 1:length(packs)) {
   if (!require(packs[i], character.only = TRUE, quietly = TRUE))
     install.packages(packs[i], character.only = TRUE)
 }
+
+# other packages work better if installed from gihtub
+
+if (!require("geobr", character.only = TRUE, quietly = TRUE))
+  devtools::install_github("ipeaGIT/geobr", subdir = "r-package")
+
+if (!require("packup", character.only = TRUE, quietly = TRUE))
+  devtools::install_github("milesmcbain/packup")
+
+if (!require("rcrossref", character.only = TRUE, quietly = TRUE))
+  remotes::install_github("ropensci/rcrossref")
+
+# load all libraries
 
 for (i in 1:length(packs)) {
   library(packs[i], character.only = TRUE)

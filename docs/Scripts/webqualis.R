5d2195262042fbc53bf1b039f7418a8479196b80
Arthur Ferreira <arthur_sf@icloud.com>
2022-09-19 07:01:59 -0300

    Commit.

--- docs/Scripts/webqualis.R
+++ docs/Scripts/webqualis.R
@@ -1,8 +1,12 @@
 # read QUALIS xlsx file (download from https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/veiculoPublicacaoQualis/listaConsultaGeralPeriodicos.jsf)
 qualis <-
-  data.frame(read_excel(
-    "Qualis/QUALIS Referência 2017-2018.xlsx",
-    sheet = 1,
-    col_types = c("text")
+  data.frame(
+    read_excel(
+      file.path(
+       "Qualis",
+       "relatorio_producao_intelectual_Qualis_Periódicos.xlsx"
+      ),
+      sheet = 1,
+      col_types = c("text")
+    )
   )
-)

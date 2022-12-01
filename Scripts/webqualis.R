43af7fd1f5999233f6e4dc7eda4fc7e08508978f
Arthur Ferreira <arthur_sf@icloud.com>
2022-12-01 16:00:53 -0300

    Commit.

--- Scripts/webqualis.R
+++ Scripts/webqualis.R
@@ -9,3 +9,13 @@ qualis <-
     sheet = 1,
     col_types = c("text")
   ))
+
+# estratos Qualis
+estratos <- sort(unique(qualis$Estrato))
+
+qualis.base <- table(qualis$Estrato) / length(qualis$Estrato) * 100
+qualis.PPG <- c()
+for(i in 1:length(estratos)){
+  qualis.PPG[i] <- sum(artigos$WebQualis == estratos[i])/length(artigos$WebQualis) * 100
+}
+names(qualis.PPG) <- estratos

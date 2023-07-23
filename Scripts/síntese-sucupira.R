548a7fa85e2ead48a6da6c86625a324b2b498616
Arthur Ferreira <arthur_sf@icloud.com>
2023-07-23 12:32:29 -0300

    Commit.

--- Scripts/síntese-sucupira.R
+++ Scripts/síntese-sucupira.R
@@ -1,11 +1,26 @@
 sintese <- function(sucupira.list = NULL, ano = NULL) {
   # initialize results
   labels <- c(
+    # 1.1.1
     'Ano',
-    'Áreas de concentração',
-    'Linhas de pesquisa',
-    'Projetos de pesquisa',
-    'Disciplinas ativas',
+    'Áreas de concentração', # OK
+    'Linhas de pesquisa', # OK
+    'Projetos de pesquisa', # OK
+    # 1.1.2
+    'Níveis de curso', # OK
+    'Mestrado', # OK
+    'Doutorado', # OK
+    'Disciplinas (M)', # OK,
+    'Disciplinas (D)', # OK,
+    'Disciplinas obrigatórias (M)', # OK
+    'Disciplinas obrigatórias (D)', # OK
+    'Disciplinas eletivas (M)', # OK
+    'Disciplinas eletivas (D)', # OK
+    'Créditos totais (M)', # OK
+    'Créditos totais (D)', # OK
+    'Carga horária total (M)', # OK
+    'Carga horária total (D)', # OK
+    # 
     'Turmas (ofertas de disciplina)',
     'Projetos de Cooperação entre Instituições',
     'Docentes permanentes',
@@ -32,13 +47,46 @@ sintese <- function(sucupira.list = NULL, ano = NULL) {
   
   sucupira <- sucupira.list[[as.character(ano)]]
   
-  # aintese estrutura curricular
+  # sintese 1.1.1 estrutura curricular
   try(resultados$'Áreas de concentração' <-
     nlevels(as.factor(sucupira$'Área de Concentração')), silent = TRUE)
   try(resultados$'Linhas de pesquisa' <-
     nlevels(as.factor(sucupira$'Linha de Pesquisa')), silent = TRUE)
   try(resultados$'Projetos de pesquisa' <-
     nlevels(as.factor(sucupira$'Nome do Projeto de Pesquisa')), silent = TRUE)
+  
+  # sintese 1.1.2 proposta curricular
+  try(resultados$'Níveis de curso' <-
+        nlevels(as.factor(sucupira$'Nível do Curso')), silent = TRUE)
+  try(resultados$'Mestrado' <-
+        nlevels(as.factor(sucupira$'Nível do Curso'[tolower(sucupira$'Nível do Curso') == "mestrado"])), silent = TRUE)
+  try(resultados$'Doutorado' <-
+        nlevels(as.factor(sucupira$'Nível do Curso'[tolower(sucupira$'Nível do Curso') == "doutorado"])), silent = TRUE)
+  
+  try(resultados$'Disciplinas (M)' <-
+        nlevels(as.factor(sucupira$'Nome da Disciplina'[tolower(sucupira$'Nível do Curso') == "mestrado"])), silent = TRUE)
+  try(resultados$'Disciplinas (D)' <-
+        nlevels(as.factor(sucupira$'Nome da Disciplina'[tolower(sucupira$'Nível do Curso') == "doutorado"])), silent = TRUE)
+  
+  try(resultados$'Disciplinas obrigatórias (M)' <-
+        sum(as.factor(sucupira$'Indicadora de disciplina obrigatória'[tolower(sucupira$'Nível do Curso') == "mestrado"]) == 'Sim'), silent = TRUE)
+  try(resultados$'Disciplinas obrigatórias (D)' <-
+        sum(as.factor(sucupira$'Indicadora de disciplina obrigatória'[tolower(sucupira$'Nível do Curso') == "doutorado"]) == 'Sim'), silent = TRUE)
+  
+  try(resultados$'Disciplinas eletivas (M)' <-
+        sum(as.factor(sucupira$'Indicadora de disciplina obrigatória'[tolower(sucupira$'Nível do Curso') == "mestrado"]) == 'Não'), silent = TRUE)
+  try(resultados$'Disciplinas eletivas (D)' <-
+        sum(as.factor(sucupira$'Indicadora de disciplina obrigatória'[tolower(sucupira$'Nível do Curso') == "doutorado"]) == 'Não'), silent = TRUE)
+  
+  try(resultados$'Créditos totais (M)' <-
+        sum(as.numeric(sucupira$'Quantidade de créditos'[tolower(sucupira$'Nível do Curso') == "mestrado"])), silent = TRUE)
+  try(resultados$'Créditos totais (D)' <-
+        sum(as.numeric(sucupira$'Quantidade de créditos'[tolower(sucupira$'Nível do Curso') == "doutorado"])), silent = TRUE)
+  
+  try(resultados$'Carga horária total (M)' <-
+        sum(as.numeric(sucupira$'Carga Horária'[tolower(sucupira$'Nível do Curso') == "mestrado"])), silent = TRUE)
+  try(resultados$'Carga horária total (D)' <-
+        sum(as.numeric(sucupira$'Carga Horária'[tolower(sucupira$'Nível do Curso') == "doutorado"])), silent = TRUE)
   
   # output
   return(resultados)
